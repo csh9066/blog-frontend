a7b77caa2873be48fa6fc6a36f4d62a254f5d7ce
seungho <50465901+csh9066@users.noreply.github.com>
2020-08-12 19:14:04 +0900

    Add app.js pages routing

--- src/App.js
+++ src/App.js
@@ -1,7 +1,21 @@
 import React from 'react';
+import { Route } from 'react-router-dom';
+import PostPage from './pages/PostPage';
+import LoginPage from './pages/LoginPage';
+import RegisterPage from './pages/RegisterPage';
+import PostListPage from './pages/PostListPage';
+import WritePage from './pages/WritePage';
 
 function App() {
-	return <div className="App">noode</div>;
+  return (
+    <>
+      <Route component={PostListPage} path={['/@:username', '/']} exact />
+      <Route component={LoginPage} path="/login" />
+      <Route component={RegisterPage} path="/register" />
+      <Route component={WritePage} path="/write" />
+      <Route component={PostPage} path="/@:username/:postId" />
+    </>
+  );
 }
 
 export default App;
