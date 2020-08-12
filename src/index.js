f7df9e3d9730e97d54f1a299220a6d7f5642ba50
seungho <50465901+csh9066@users.noreply.github.com>
2020-08-12 19:20:24 +0900

    Add index.js redux and router

--- src/index.js
+++ src/index.js
@@ -1,10 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import { createStore } from 'redux';
+import { composeWithDevTools } from 'redux-devtools-extension';
+import { BrowserRouter } from 'react-router-dom';
+
 import App from './App';
+import rootReducer from './modules/index';
+
+const store = createStore(rootReducer, composeWithDevTools());
 
 ReactDOM.render(
-	<React.StrictMode>
-		<App />
-	</React.StrictMode>,
-	document.getElementById('root')
+  <Provider store={store}>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </Provider>,
+  document.getElementById('root'),
 );
