047ded3b3cbbfab3448946afc28516b45259ed64
seungho <50465901+csh9066@users.noreply.github.com>
2020-08-13 19:32:15 +0900

    Add app.js GlobalStyle

--- src/App.js
+++ src/App.js
@@ -5,10 +5,34 @@ import LoginPage from './pages/LoginPage';
 import RegisterPage from './pages/RegisterPage';
 import PostListPage from './pages/PostListPage';
 import WritePage from './pages/WritePage';
+import { createGlobalStyle } from 'styled-components';
+
+const GlobalStyle = createGlobalStyle`
+  body {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+    min-height: 100%;
+  }
+
+  #root {
+    min-height: 100%;
+  }
+
+  html {
+    height: 100%
+  }
+
+  a {
+    color: inherit;
+    text-decoration:none;
+  }
+`;
 
 function App() {
   return (
     <>
+      <GlobalStyle />
       <Route component={PostListPage} path={['/@:username', '/']} exact />
       <Route component={LoginPage} path="/login" />
       <Route component={RegisterPage} path="/register" />
