b10c19cb09ecc7c4c9ed317f7f4652d560c8f528
seungho <50465901+csh9066@users.noreply.github.com>
2020-08-13 19:31:34 +0900

    Add pages

---
+++ src/pages/LoginPage.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import AuthTemplate from '../components/auth/AuthTemplate';
+import AuthForm from '../components/auth/AuthForm';
+
+const LoginPage = () => {
+  return (
+    <AuthTemplate>
+      <AuthForm type="login" />
+    </AuthTemplate>
+  );
+};
+export default LoginPage;
---
+++ src/pages/PostListPage.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import Button from '../components/common/Button';
+
+const PostListPage = () => {
+  return (
+    <div>
+      <Button>안뇽</Button>
+    </div>
+  );
+};
+export default PostListPage;
