6295290a3fdf58609026e0405e960b539a633456
seungho <50465901+csh9066@users.noreply.github.com>
2020-08-18 19:53:37 +0900

    Update redux auth module

--- src/modules/auth.js
+++ src/modules/auth.js
@@ -1,15 +1,36 @@
 import { createAction, handleActions } from 'redux-actions';
 import produce from 'immer';
+import createRequestSaga, {
+  createRequestType,
+} from '../lib/utils/createRequestSaga';
+
+import * as authApi from '../lib/api/auth';
+import { takeLatest } from 'redux-saga/effects';
 
 const CHNAGE_FILED = 'auth/CHANGE_FIELD';
 const INITIALIZE_FORM = 'auth/INITIALIZE_FORM';
+
+const [REGISTER, REGISTER_SUCCESS, REGISTER_FAILURE] = createRequestType(
+  'auth/REGISTER',
+);
+const [LOGIN, LOGIN_SUCCESS, LOGIN_FAILURE] = createRequestType('auth/LOGIN');
 
 export const changeField = createAction(
   CHNAGE_FILED,
   ({ form, key, value }) => ({ form, key, value }),
 );
-
 export const initializeForm = createAction(INITIALIZE_FORM, (form) => form);
+export const register = createAction(REGISTER, ({ username, password }) => ({
+  username,
+  password,
+}));
+export const login = createAction(LOGIN, ({ username, password }) => ({
+  username,
+  password,
+}));
+
+export const registerSaga = createRequestSaga(REGISTER, authApi.register);
+export const loginSaga = createRequestSaga(LOGIN, authApi.login);
 
 const initialState = {
   register: {
@@ -21,7 +42,14 @@ const initialState = {
     username: '',
     password: '',
   },
+  auth: null,
+  authError: null,
 };
+
+export function* authSaga() {
+  yield takeLatest(REGISTER, registerSaga);
+  yield takeLatest(LOGIN, loginSaga);
+}
 
 const auth = handleActions(
   {
@@ -32,6 +60,25 @@ const auth = handleActions(
     [INITIALIZE_FORM]: (state, { payload: form }) =>
       produce(state, (draft) => {
         draft[form] = initialState[form];
+        draft.authError = null;
+      }),
+    [REGISTER_SUCCESS]: (state, { payload: auth }) =>
+      produce(state, (draft) => {
+        draft.authError = null;
+        draft.auth = auth;
+      }),
+    [REGISTER_FAILURE]: (state, { payload: error }) =>
+      produce(state, (draft) => {
+        draft.authError = error;
+      }),
+    [LOGIN_SUCCESS]: (state, { payload: auth }) =>
+      produce(state, (draft) => {
+        draft.authError = null;
+        draft.auth = auth;
+      }),
+    [LOGIN_FAILURE]: (state, { payload: error }) =>
+      produce(state, (draft) => {
+        draft.authError = error;
       }),
   },
   initialState,
