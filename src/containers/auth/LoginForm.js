e0cc1c80b46f3ff43484cb35b1877531f8689174
seungho <50465901+csh9066@users.noreply.github.com>
2020-08-21 13:55:32 +0900

    Update LoginForm

--- src/containers/auth/LoginForm.js
+++ src/containers/auth/LoginForm.js
@@ -40,6 +40,11 @@ const LoginForm = () => {
   useEffect(() => {
     if (user) {
       history.push('/');
+      try {
+        localStorage.setItem('user', JSON.stringify(user));
+      } catch (e) {
+        console.log('localStroage not working');
+      }
     }
   }, [user, history]);
 
