3843bc3d3ca3789f93bfddb10e2908e06a66189f
seungho <50465901+csh9066@users.noreply.github.com>
2020-08-26 19:16:51 +0900

    Update posts api

--- src/lib/api/posts.js
+++ src/lib/api/posts.js
@@ -1,6 +1,21 @@
+import qs from 'qs';
 import client from './client';
 
 export const writePost = ({ title, body, tags }) =>
   client.post('/api/posts', { title, body, tags });
 
 export const readPost = (id) => client.get(`/api/posts/${id}`);
+
+export const listPosts = ({ page, username, tag }) => {
+  const queryString = qs.stringify({
+    page,
+    username,
+    tag,
+  });
+  return client.get(`/api/posts?${queryString}`);
+};
+
+export const updatePost = ({ title, body, tags, id }) =>
+  client.patch(`/api/posts/${id}`, { title, body, tags });
+
+export const removePost = (id) => client.delete(`/api/posts/${id}`);
